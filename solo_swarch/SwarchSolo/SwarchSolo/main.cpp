da15484937c474772d5031a6d748a8dc1f27f45c
JordanLingHua <jordan.hua@yahoo.com>
2014-04-12 12:51:33 -0700

    Sync with important description
    Jordan going to get started on:
    
    -Display size in front of mybox and have it move with mybox
    -Pellet generator (random pellet in random location; when less than 4
    pellets, spawn a pellet (while < 4 pellets, keep spawning pellets)
    
    <---Just a list of what I will start.  Going to write this per
    significant commit.  Tasks I write will be done with Jonathan when he
    comes online.  Will start base code what to do.  May need skype on.  Or
    use cell phone like skype.....  update soon

--- solo_swarch/SwarchSolo/SwarchSolo/main.cpp
+++ solo_swarch/SwarchSolo/SwarchSolo/main.cpp
@@ -2,9 +2,20 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //Set up like a unity c# file as much as possible. 
+	//Avoid making header files to call things IF POSSIBLE.  
+	//May need it for:  ..
+
+
+	//Initialize.  Can change in loop.  
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Swarch");
+	sf::RectangleShape myBox(sf::Vector2f(10,10));
+	myBox.setFillColor(sf::Color::Cyan);
+
+	float mySpeed = 0.1f;
+
+
+	//Game loop (actual main)
 
     while (window.isOpen())
     {
@@ -16,8 +27,46 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(myBox);
         window.display();
+
+
+		//Game Logic start here:  
+
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			myBox.move(0, -mySpeed);
+		}
+		else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			myBox.move(0, mySpeed);
+		}
+		else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			myBox.move(mySpeed, 0);
+		}
+		else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			myBox.move(-mySpeed, 0);
+		}
+
+
+
+
+		//while(numPellets < 4) spawnPellet(pellet, randomLocation);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
     return 0;
