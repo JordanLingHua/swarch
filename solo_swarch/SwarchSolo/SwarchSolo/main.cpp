a291e3b3e7ac837cd0509915d90b6e4a4605f1da
jkwendorf <jkwendorf>
2014-04-24 15:35:10 -0700

    Added comments to our code
    We will need to add two new projects for client and server coding.

--- solo_swarch/SwarchSolo/SwarchSolo/main.cpp
+++ solo_swarch/SwarchSolo/SwarchSolo/main.cpp
@@ -8,26 +8,32 @@ int main()
 	//Avoid making header files to call things IF POSSIBLE.  
 	//May need it for:  ..
 
-
 	//Initialize.  Can change in loop.
-
+	
+	// Preset the window size
 	int windowSizeX = 500;
 	int windowSizeY = 500;
 
+	// Set the text box sizes
 	int textBoxX = 300;
 	int textBoxY = 50;
 
+	// Set the size of the initial swarch
 	int myBoxSizeX = 10;
 	int myBoxSizeY = 10;
 	//int myBoxSize = 10;//comment out.  Just send only myBoxSizeX
 	//since myBoxSizeX is == myBoxSizeY.  When receive, 
 	//give myBoxSizeX and myBoxSizeY the same value packet (whatever it's called)
 
-	// Create GUI for login
+// Create GUI for login
 	sf::Font font;
+	// We must load the font as SFML doesn't contain a default font anymore
 	font.loadFromFile("arial.ttf");
+	// Create strings and sf::Texts to enable text to be written on the window
 	std::string user, password;
 	sf::Text userT, passwordT;
+
+	// Set the font, set the origin and set the color
 	userT.setFont(font);
 	passwordT.setFont(font);
 	userT.setOrigin(sf::Vector2f(0, userT.getLocalBounds().height/2));
@@ -71,9 +77,11 @@ int main()
 	userBox.setPosition(sf::Vector2f(userTitle.getGlobalBounds().width + 20, 210));
 	passBox.setPosition(sf::Vector2f(passTitle.getGlobalBounds().width + 20, 310));
 
+	// Set the position of the text on top of each box
 	userT.setPosition(userBox.getGlobalBounds().left + 5, userBox.getGlobalBounds().top);
 	passwordT.setPosition(passBox.getGlobalBounds().left + 5, passBox.getGlobalBounds().top);
 
+	// Create the "enter" button and set its position
 	sf::Texture texture;
 	texture.loadFromFile("swarchEnter.png");
 	sf::Sprite enterButton;
@@ -81,28 +89,34 @@ int main()
 	enterButton.setOrigin(sf::Vector2f(enterButton.getLocalBounds().width/2, enterButton.getLocalBounds().height/2));
 	enterButton.setPosition(400 ,passBox.getGlobalBounds().top + passBox.getLocalBounds().height/2 + 80);
 
+	// Wait for the user to enter a name and password
 	bool nameAndPasswordValid = false;
 	bool enterUser = true, enterPass = false;
 	while(window.isOpen() && !nameAndPasswordValid)
 	{
+		// Set the text to whatever the user enters
 		userT.setString(user);
 		passwordT.setString(password);
 
 		sf::Event event;
 		while(window.pollEvent(event))
 		{
+			// If the user tries to close the window, end the simulation
 			if(event.type == sf::Event::Closed)
 			{
 					window.close();
 			}
+			// If the user presses a key
 			else if(event.type == sf::Event::KeyPressed)
 			{
+				// Was the key Return?
 				if(event.key.code == sf::Keyboard::Return)
 				{
 					// We must check to see if the password and name are valid, but for now this will suffice
 					nameAndPasswordValid = true;
 					userName.setString(user);
 				}
+				// Was the key BackSpace?
 				else if(event.key.code == sf::Keyboard::BackSpace)
 				{
 					// If user selected, delete from user
@@ -113,8 +127,10 @@ int main()
 						password = password.substr(0, password.size() - 1);
 				}
 			}
+			// Enter the text
 			else if(event.type == sf::Event::TextEntered)
 			{
+				// If the text isn't a backspace, return or delete key
 				if(event.text.unicode != 8 && event.text.unicode != 15 && event.text.unicode != 127)
 				{
 					if(enterUser)
@@ -123,6 +139,7 @@ int main()
 						password += event.text.unicode;
 				}
 			}
+			// If the mouse is pressed, select the box pressed on
 			else if(event.type == sf::Event::MouseButtonPressed)
 			{
 				sf::Vector2i mousePos = sf::Mouse::getPosition(window);
@@ -146,7 +163,7 @@ int main()
 			}
 		}
 		
-		
+		// Empty the window
 		window.clear(sf::Color(0, 0, 64));
 
 		// Draw the Login GUI
@@ -162,6 +179,7 @@ int main()
 		window.display();
 	}
 
+	// Preset values for the initial game
 	float mySpeed = 200.f;
 	float delay = 0;
 	int randPelletLocX;
@@ -171,21 +189,16 @@ int main()
 
 	int numOfPellets = 4;
 
-	//All these types of ways to make arrays work:
-	//sf::RectangleShape* pelletList;
-	//sf::RectangleShape pelletList[4];
+	// Create an array of pellets
 	sf::RectangleShape* pelletList = new sf::RectangleShape[numOfPellets];
 
-	
+	// Fill the array will "pellets"
 	for(int i = 0; i < numOfPellets; i++)
 	{
 		pelletList[i] =  sf::RectangleShape(sf::Vector2f(5,5));
 		pelletList[i].setFillColor(sf::Color::White);
 
-		
-		//window.draw(pelletList[i]);
-
-
+		// Choose a random location
         randPelletLocX = 1+rand()%(windowSizeX);
 	    randPelletLocY = 1+rand()%(windowSizeY);
 
@@ -194,11 +207,15 @@ int main()
 
 	//Game loop (actual main)
 
+	// Start the delta timer
 	sf::Clock deltaTimer;
 	sf::Time lastFrameTime;
 
+	// While the game is being played
     while (window.isOpen())
     {
+		// The delta time is the time between frames
+		// Delta time ensures that actions are consistent between users
 		float deltaTime = (deltaTimer.getElapsedTime() - lastFrameTime).asSeconds();
 		lastFrameTime = deltaTimer.getElapsedTime();
 
@@ -275,6 +292,7 @@ int main()
 		}
 	
 		// Not a good delay as speeds may vary between computers
+		// We are using the TA's implementation
 		delay -= 4;
 
 		if(delay <= 0)
@@ -283,7 +301,7 @@ int main()
 			delay = (myBox.getLocalBounds().width - 10) / 2;
 		}
     }
-
+	// Free any allocated resources
 	delete[] pelletList;
 
     return 0;
